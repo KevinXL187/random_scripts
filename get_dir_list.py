8cb5fdce6dc735749f958ce5f74c80b93afd7724
Kevin <XiongL.QiXing@gmail.com>
2024-03-29 16:19:42 -0400

    Create script to get list of all dir/files in a directory

---
+++ get_dir_list.py
@@ -0,0 +1,19 @@
+import os, sys
+
+# gets all directories in a folder and write them to a text file
+def write_dirs_to_file(folder_path, output_file):
+    with open(output_file, 'w') as f:
+        for root, dirs, files in os.walk(folder_path):
+            for directory in dirs:
+                dir_path = os.path.join(root, directory)
+                f.write(f"{directory}: {dir_path}\n")
+    
+    # print("Directories written to", output_file)
+
+if __name__ == "__main__":
+    try:
+        folder_path, output_file = sys.argv[1], sys.argv[2]
+    except IndexError:
+        print("Error: missing necessary arguments to run program")
+        print("Input: folder_path, output_file")
+        sys.exit(1)
