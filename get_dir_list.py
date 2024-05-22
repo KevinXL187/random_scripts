5277c573ac3401221c30949f639abd989d3b4299
Kevin <XiongL.QiXing@gmail.com>
2024-05-22 12:57:31 -0400

    feat: added new function to get only the top layer of folders in a directory

--- get_dir_list.py
+++ get_dir_list.py
@@ -10,11 +10,18 @@ def write_dirs_to_file(folder_path, output_file):
     
     print("Directories written to", output_file)
 
+def sup_dirs_to_file(folder_path, output_file):
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for item in os.listdir(folder_path):
+            if os.path.isdir(item):
+                f.write(f"{item} : {os.path.join(folder_path, item)}\n")
+
 if __name__ == "__main__":
-    folder_path, output_file = sys.argv[1], sys.argv[2]
+    folder_path, output_file, mode = sys.argv[1], sys.argv[2], sys.argv[3]
     try:
-        write_dirs_to_file(folder_path, output_file)
+        if mode == '0': write_dirs_to_file(folder_path, output_file)
+        elif mode == '1': sup_dirs_to_file(folder_path, output_file)
     except IndexError:
         print("Error: missing necessary arguments to run program")
-        print("Input: folder_path, output_file")
+        print("Input: folder_path, output_file, mode")
         sys.exit(1)
