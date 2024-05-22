a19d88099a3fa99ab8ddb63d798d9be1a5fd83f9
Kevin <XiongL.QiXing@gmail.com>
2024-05-22 12:47:24 -0400

    fix: called the actual function and utf-8 encoding support

--- get_dir_list.py
+++ get_dir_list.py
@@ -2,17 +2,18 @@ import os, sys
 
 # gets all directories in a folder and write them to a text file
 def write_dirs_to_file(folder_path, output_file):
-    with open(output_file, 'w') as f:
+    with open(output_file, 'w', encoding='utf-8') as f:
         for root, dirs, files in os.walk(folder_path):
             for directory in dirs:
                 dir_path = os.path.join(root, directory)
                 f.write(f"{directory}: {dir_path}\n")
     
-    # print("Directories written to", output_file)
+    print("Directories written to", output_file)
 
 if __name__ == "__main__":
+    folder_path, output_file = sys.argv[1], sys.argv[2]
     try:
-        folder_path, output_file = sys.argv[1], sys.argv[2]
+        write_dirs_to_file(folder_path, output_file)
     except IndexError:
         print("Error: missing necessary arguments to run program")
         print("Input: folder_path, output_file")
