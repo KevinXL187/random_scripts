d83a395888d0949e87251348d5e20e51d5b6963a
Kevin <XiongL.QiXing@gmail.com>
2024-08-28 16:58:34 -0400

    feat: made the skeleton code

--- get_playlist.py
+++ get_playlist.py
@@ -1,3 +1,16 @@
-
 # get a playlist from spotify using Spotify Web API
 def spotify_playlist():
+    pass
+
+# compare spotify playlist to a given list of songs based on artist and title
+def compare_playlist():
+    # when comparing songs with chinese characters, make sure to translate everything to simplified characters for both playlist
+    pass
+
+    # if song not in given playlist, record it
+    def record_song():
+        pass
+
+# export recorded list of missing songs
+def export_list():
+    pass
