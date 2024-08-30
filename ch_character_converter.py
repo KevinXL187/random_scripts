f67d455092c990cf6d3a224f1944e02e26690e7e
Kevin <XiongL.QiXing@gmail.com>
2024-08-30 14:57:05 -0400

    feat: created skeleton code

---
+++ ch_character_converter.py
@@ -0,0 +1,14 @@
+import unicodedata
+
+# character dectection (simplified, tradtional)
+def char_detection(char_ch):
+   encoding_bytes = char_ch.encode("utf-16")
+   
+
+# convert to simplified
+def trad_simp(char_ch):
+    pass
+
+# convert to traditional
+def simp_trad(char_ch):
+    pass
