a8ca65d91b20361cb888a29a063d0a772c2e6667
Kevin <XiongL.QiXing@gmail.com>
2025-01-07 23:12:45 -0500

    feat: added function for chinese character conversion from traditional to simplified and vice versea, and character type dectection

--- ch_character_converter.py
+++ ch_character_converter.py
@@ -1,14 +1,21 @@
-import unicodedata
+import opencc
+from pyunihan import Query
 
 # character dectection (simplified, tradtional)
 def char_detection(char_ch):
-   encoding_bytes = char_ch.encode("utf-16")
+    result = Query(char_ch)
+    if 'kSimplifiedVariant' in result: return "Simplified"
+    elif 'kTraditionalVariant' in result: return "Traditional"
+    else: return "Unknown or Mixed"
    
-
 # convert to simplified
-def trad_simp(char_ch):
-    pass
+def trad_simp(text):
+    t2s = opencc.OpenCC('t2s')
+    simplified = t2s.convert(text)
+    return simplified
 
 # convert to traditional
-def simp_trad(char_ch):
-    pass
+def simp_trad(text):
+    s2t = opencc.OpenCC('s2t')
+    traditional = s2t.convert(text)
+    return traditional
